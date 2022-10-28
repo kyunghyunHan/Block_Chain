fe55ba21604e83353cf662f467f2c9eab1e19ffc
kyunghyunHan <ekfqlwcjswl@naver.com>
2022-10-28 10:45:52 +0900

    ㅁㅇㅇ

--- src/blocks/block.rs
+++ src/blocks/block.rs
@@ -10,9 +10,10 @@ use crate::{
 
 /*
 블록헤더
-타임스탬프:시간
+타임스탬프:블록이 생성된 대략적인 시간,타임스탬프에 의존하고 현재 평균 블록 생성 비율이 목표 값과 얼마나 잘 일치하는지 결정하는 스마트 계약에서 사용
 prev_hash:이전시간
 nonce:작업증명을 계산하는데 사용하는 난수
+txs_hash:트랜잭션 집합의 해시 값
 bits:난이도,즉 블록 해시 값의 첫번쨰 비트가 0인수를 계산
 nonce:비트의 난이도를 충족시키기 위해 계산이 반복되는 횟수
  */
@@ -71,6 +72,10 @@ impl Block {
     }
     //제네시스 블록 구현
     //첫번쨰 블록이므로 블록헤더의 prev_hash에는 값이 없다.
+    //제네시스 블록 생성 시 코인베이스 트랜잭션 생성
+    //혹은 generation transaction
+    //코인베이스 트랜잭션(coinbase transaction)은 새 블록의 첫 거래이고, 코인베이스(coinbase)는 이 거래의 '입력' 의 내용물
+    //base기반
     pub fn create_genesis_block(bits: usize, genesis_addr: &str) -> Self {
         let coinbase = Transaction::new_coinbase(genesis_addr);
         Self::new(&vec![coinbase], "", bits)
